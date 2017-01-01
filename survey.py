5a273380509fe5ed5f584f6854b67a8ee8fd6960
Oliver Newman <oliver@whmsi.com>
2016-12-31 23:26:23 -0500

    Implement csv writing

--- survey.py
+++ survey.py
@@ -1,21 +1,27 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from random import randrange
-import datetime as dt
+from datetime import datetime as dt
+import csv
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def survey():
     if request.method == 'POST':
-        time = dt.now()
+        time = str(dt.now())
+        admin = request.form['admin']
         location = request.form['location']
         subject_id = randrange(999999)
         subject_age = request.form['subject_age']
         subject_gender = request.form['subject_gender']
-        answers = []
-        for i in range(10):
-            # TODO: might need to use explicit 'true'/'false' here
-            phone = 'Yes' if request.form['phone{}'.format(i)] else 'No'
-            answers.append((phone, request.form['answer{}'.format(i)]))
-    else:
-        return render_template('survey.html')
+        with open('friendliness_data.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            for i in range(10):
+                # TODO: might need to use explicit 'true'/'false' here
+                # phone = 'Yes' if request.form['phone{}'.format(i)] else 'No'
+                phone = 'Phone'
+                answer = request.form['answer{}'.format(i)]
+                writer.writerow([subject_id, subject_age, subject_gender, time,
+                                 location, admin, i, phone, answer])
+
+    return render_template('survey.html')
