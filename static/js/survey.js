1d0ac532b92fb8399a63c397556647585a2ca01d
Oliver Newman <oliver@whmsi.com>
2016-12-31 23:26:12 -0500

    Implement basic front-end survey functionality

--- static/js/survey.js
+++ static/js/survey.js
@@ -1,16 +1,42 @@
+// true if phone, false if no phone
+const SET_A = [true, false, false, true, false, true, true, true, false, false];
+
 $(function () {
-  $('#next-button').click(function () {
-    $('#initial-form').hide();
-    showQuestion(0)
+  $("#submit-button").attr("disabled", false);
+  const set_a = false;
+  for (let i = 0; i < 10; i++) {
+    const dir = (set_a ? !SET_A[i] : SET_A[i]) ? "phone" : "no_phone";
+    $(`#image${i}`).append(`<img src="static/images/${dir}/${i}.jpg">`);
+  }
+
+  // $("#next-button").attr("disabled", true);
+
+  $("#next-button").click(function () {
+    $("#initial-form").hide();
+    console.log("original listener fired");
+    $("#next-button").unbind();
+    showQuestion(0);
   });
 });
 
 function showQuestion(num) {
-  $(`#form${num}`).show();
-  $('#next-button').click(function() {
-    $(`#form${num}`).hide();
+  $("#next-button").hide();
+  $(`#image${num}`).show();
+  setTimeout(function () {
+    $(`#image${num}`).hide();
+    $(`#question${num}`).show();
+    $('#next-button').show();
+  }, 5000);
+  $("#next-button").unbind();
+  $("#next-button").click(function() {
+    $(`#question${num}`).hide();
     if (num < 9) {
       showQuestion(num + 1);
+    }
+    else {
+      $("#next-button").hide();
+      $("#submit-container").show();
+      $("#submit-button").attr("disabled", false);
     }
   });
 }
